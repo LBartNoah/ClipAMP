87833afa4a618d80f375552999077a9f293ca1bc
Noah Bartley <75696241+LBartNoah@users.noreply.github.com>
2020-12-08 11:42:12 -0800

    Source Files
    Updated Source Files
    ClipAMP Version 0.0.0.1

---
+++ ClipAMP/GUI.Designer.cs
@@ -0,0 +1,154 @@
+﻿
+namespace ClipAMP
+{
+    partial class GUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GUI));
+            this.OutputController = new System.Windows.Forms.GroupBox();
+            this.NoSpaceCheck = new System.Windows.Forms.CheckBox();
+            this.ShowWAMark = new System.Windows.Forms.CheckBox();
+            this.SongOutputBox = new System.Windows.Forms.TextBox();
+            this.HideButton = new System.Windows.Forms.Button();
+            this.UpdateMethod = new System.Windows.Forms.Timer(this.components);
+            this.PoweredByText = new System.Windows.Forms.Label();
+            this.WhippingText = new System.Windows.Forms.Label();
+            this.OutputController.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // OutputController
+            // 
+            this.OutputController.Controls.Add(this.WhippingText);
+            this.OutputController.Controls.Add(this.PoweredByText);
+            this.OutputController.Controls.Add(this.NoSpaceCheck);
+            this.OutputController.Controls.Add(this.ShowWAMark);
+            this.OutputController.Controls.Add(this.SongOutputBox);
+            this.OutputController.Location = new System.Drawing.Point(12, 12);
+            this.OutputController.Name = "OutputController";
+            this.OutputController.Size = new System.Drawing.Size(195, 194);
+            this.OutputController.TabIndex = 0;
+            this.OutputController.TabStop = false;
+            this.OutputController.Text = "Output Settings";
+            // 
+            // NoSpaceCheck
+            // 
+            this.NoSpaceCheck.AutoSize = true;
+            this.NoSpaceCheck.Location = new System.Drawing.Point(6, 80);
+            this.NoSpaceCheck.Name = "NoSpaceCheck";
+            this.NoSpaceCheck.Size = new System.Drawing.Size(105, 17);
+            this.NoSpaceCheck.TabIndex = 3;
+            this.NoSpaceCheck.Text = "Remove Spaces";
+            this.NoSpaceCheck.UseVisualStyleBackColor = true;
+            // 
+            // ShowWAMark
+            // 
+            this.ShowWAMark.AutoSize = true;
+            this.ShowWAMark.Location = new System.Drawing.Point(6, 57);
+            this.ShowWAMark.Name = "ShowWAMark";
+            this.ShowWAMark.Size = new System.Drawing.Size(150, 17);
+            this.ShowWAMark.TabIndex = 2;
+            this.ShowWAMark.Text = "Show Winamp Watermark";
+            this.ShowWAMark.UseVisualStyleBackColor = true;
+            // 
+            // SongOutputBox
+            // 
+            this.SongOutputBox.Location = new System.Drawing.Point(6, 31);
+            this.SongOutputBox.Name = "SongOutputBox";
+            this.SongOutputBox.ReadOnly = true;
+            this.SongOutputBox.Size = new System.Drawing.Size(183, 20);
+            this.SongOutputBox.TabIndex = 2;
+            this.SongOutputBox.Text = "Song Output....";
+            // 
+            // HideButton
+            // 
+            this.HideButton.Location = new System.Drawing.Point(213, 17);
+            this.HideButton.Name = "HideButton";
+            this.HideButton.Size = new System.Drawing.Size(109, 189);
+            this.HideButton.TabIndex = 1;
+            this.HideButton.Text = "Hide ClipAMP";
+            this.HideButton.UseVisualStyleBackColor = true;
+            this.HideButton.Click += new System.EventHandler(this.HideButton_Click);
+            // 
+            // UpdateMethod
+            // 
+            this.UpdateMethod.Interval = 1;
+            this.UpdateMethod.Tick += new System.EventHandler(this.UpdateMethod_Tick);
+            // 
+            // PoweredByText
+            // 
+            this.PoweredByText.AutoSize = true;
+            this.PoweredByText.Location = new System.Drawing.Point(54, 111);
+            this.PoweredByText.Name = "PoweredByText";
+            this.PoweredByText.Size = new System.Drawing.Size(80, 39);
+            this.PoweredByText.TabIndex = 2;
+            this.PoweredByText.Text = "Powered By:\r\n- Lots Of Soda\r\n- Snack Breaks";
+            this.PoweredByText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // WhippingText
+            // 
+            this.WhippingText.AutoSize = true;
+            this.WhippingText.Location = new System.Drawing.Point(15, 159);
+            this.WhippingText.Name = "WhippingText";
+            this.WhippingText.Size = new System.Drawing.Size(164, 13);
+            this.WhippingText.TabIndex = 4;
+            this.WhippingText.Text = "Guaranteed to whip the llama\'s....";
+            // 
+            // GUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 218);
+            this.Controls.Add(this.HideButton);
+            this.Controls.Add(this.OutputController);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "GUI";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ClipAMP";
+            this.Load += new System.EventHandler(this.GUI_Load);
+            this.OutputController.ResumeLayout(false);
+            this.OutputController.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox OutputController;
+        private System.Windows.Forms.Button HideButton;
+        private System.Windows.Forms.Timer UpdateMethod;
+        private System.Windows.Forms.TextBox SongOutputBox;
+        private System.Windows.Forms.CheckBox NoSpaceCheck;
+        private System.Windows.Forms.CheckBox ShowWAMark;
+        private System.Windows.Forms.Label WhippingText;
+        private System.Windows.Forms.Label PoweredByText;
+    }
+}
+
---
+++ ClipAMP/GUI.cs
@@ -0,0 +1,100 @@
+﻿// ClipAMP
+// Version 0.0.0.1
+// Noah Bartley
+
+// ClipAMP is licensed under the GPL-3.0 License.
+// You can find the ClipAMP repository on GitHub here:
+// https://github.com/LBartNoah
+// https://github.com/LBartNoah/ClipAMP
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace ClipAMP
+{
+    public partial class GUI : Form
+    {
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern IntPtr FindWindow(string ClassName, string WindowName);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern int GetWindowText(IntPtr HWindow, String String, int MaxCount);
+
+        public GUI()
+        {
+            InitializeComponent();
+        }
+
+        private void GUI_Load(object sender, EventArgs e)
+        {
+            if(File.Exists("EasterEggs.txt") == true)
+            {
+                MessageBox.Show("Noah Bartley Presents\nClipAMP \nVersion 0.0.0.1 \n \nThe amazing and constantly updated program that lets you view Winamp's currently playing song and place it into a simple text file!\n\nThank you for using this program. We hope you enjoy it as much as we did making it!", "You Unlocked An Easter Egg!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                MessageBox.Show("How did you find this anyways.....\nOh well enjoy the program now.\n\n\nYou can remove this message from startup by removing the EasterEggs text file.", "Wait A Minute!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            ShowWAMark.Checked = false;
+            NoSpaceCheck.Checked = false;
+            if(File.Exists("WinampSong.txt") == false)
+            {
+                try
+                {
+                    File.CreateText("WinampSong.txt");
+                    UpdateMethod.Start();
+                }
+                catch
+                {
+                    MessageBox.Show("Failed to create Winamp Song text file. Try running as administrator and make sure the path the executable is in has valid access.", "Failed To Create File", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                UpdateMethod.Start();
+            }
+        }
+
+        private void HideButton_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void UpdateMethod_Tick(object sender, EventArgs e)
+        {
+            IntPtr WAWindow = FindWindow("Winamp v1.x", null);
+            if(WAWindow.Equals(IntPtr.Zero))
+            {
+                SongOutputBox.Text = "";
+            }
+            else
+            {
+                string SongName = new string((char)0, 100);
+                int WindowText = GetWindowText(WAWindow, SongName, SongName.Length);
+                if(ShowWAMark.Checked == true && NoSpaceCheck.Checked == true)
+                {
+                    SongOutputBox.Text = SongName.Replace(" ", string.Empty).ToString();
+                }
+                if (ShowWAMark.Checked == true && NoSpaceCheck.Checked == false)
+                {
+                    SongOutputBox.Text = SongName.ToString();
+                }
+                if (ShowWAMark.Checked == false && NoSpaceCheck.Checked == true)
+                {
+                    SongOutputBox.Text = SongName.Replace("- Winamp", string.Empty).Replace(" ", string.Empty).ToString();
+                }
+                if (ShowWAMark.Checked == false && NoSpaceCheck.Checked == false)
+                {
+                    SongOutputBox.Text = SongName.Replace("- Winamp", string.Empty).ToString();
+                }
+                File.WriteAllText("WinampSong.txt", SongOutputBox.Text.ToString());
+             }
+         }
+    }
+}
